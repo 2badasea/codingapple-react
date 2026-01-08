4bb13af8d79955ecbb9eabc48fc0e7912820652d
이바다 <gnjqtpf1@gmail.com>
2026-01-08 23:50:03 +0900

    useState와 함수 컴포넌트를 활용하여 화면에 데이터 표시

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,9 +1,38 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css'; // css 파일 쓰려면 상단에서 import 'css 파일 경로'
 
-function App() {
-  return (
-    <div></div>
-  )
+function PostContent({ id, title, writeDate }) {
+	return (
+		<div className="list" key={id}>
+			<h4>{title}</h4>
+			<p>{writeDate}</p>
+		</div>
+	);
 }
 
-export default App
+function App() {
+	let logo = 'React 숙제';
+	const [contents] = useState([
+		{ id: 1, title: '첫 번째 제목', writeDate: '1월 6일' },
+		{ id: 2, title: '두 번째 제목', writeDate: '1월 7일' },
+		{ id: 3, title: '세 번째 제목', writeDate: '1월 8일' },
+		{ id: 4, title: '네 번째 제목', writeDate: '1월 9일' },
+		{ id: 5, title: '다섯 번째 제목', writeDate: '1월 10일' },
+	]);
+
+	return (
+		<div className="App">
+			<div className="black-nav">
+				<h4>{logo}</h4>
+			</div>
+
+			{contents.map((row) => {
+        return (
+          <PostContent key={row.id} title={row.title} writeDate={row.writeDate} />
+        )
+			})}
+		</div>
+	);
+}
+
+export default App;
